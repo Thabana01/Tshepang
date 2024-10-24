9acb2385a062c00938a2d7c8023018072d859d90
Thabana01 <tsepang331@gmail.com>
2024-10-24 03:32:46 -0700

    Add files via upload

---
+++ Tsepang Thabana.ccp.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int A,B,C;
+	int average;
+	cout<<"enter the 1st number:";
+	cin	>>A;
+	cout<<"enter the 2nd number:";
+	cin>>B;
+	cout<<"enter the 3rd number:";
+	cin>>C;
+   average =(A+B+C)/3;
+cout<<"average:"<<average<<endl;	
+return 0;	
+}
